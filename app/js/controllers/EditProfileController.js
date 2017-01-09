0bfb22b0feae00d4388dedeea7232147e7c95ab4
Rahul Jain <rjain6@buffalo.edu>
2017-01-08 21:19:06 -0500

    more learning...

---
+++ app/js/controllers/EditProfileController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+eventsApp.controller('EditProfileController', function($scope, gravatarUrlBuilder){
+    $scope.user = {};
+
+    $scope.getGravatarUrl = function(email){
+        return gravatarUrlBuilder.getGravatarUrl(email);
+    }
+
+
+});
--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -4,35 +4,23 @@
 
 'use strict';
 
-eventsApp.controller('EventController', function EventController($scope){
-    $scope.message = "Hello world!";
-    $scope.boolVal=true;
-    $scope.myStyle={color:'red'};
-    $scope.myClass = "blue";
-    $scope.myButton = false;
-    $scope.sortOrder='name';
-    $scope.event = {
-        name: 'Angular Demo App',
-        date: '12/24/2016',
-        time: '5.00pm',
-        modules:[
-            {
-                name: 'Controllers and Markup',
-                duration: 1,
-                upVoteCount: 0
-            },
-            {
-                name: 'Angular Routing',
-                duration: 2,
-                upVoteCount: 0
+eventsApp.controller('EventController',
+    function EventController($scope, eventData, $log) {
+        $scope.message = "Hello world!";
+        $scope.boolVal = true;
+        $scope.myStyle = {color: 'red'};
+        $scope.myClass = "blue";
+        $scope.myButton = false;
+        $scope.sortOrder = 'name';
+        $scope.snippet = '<span>hi there!</span>';
+        eventData.getEvent()
+            .success(function(event, status, headers, config){
+                $log.warn(event, status, headers(), config);
+                $scope.event = event;
+            })
+            .error(function(data, status, headers, config){
+                    $log.warn(data, status, headers(), config)
+            });
 
-            },
-            {
-                name: 'Angular Directives',
-                duration: 3,
-                upVoteCount: 0
-            }
-        ]
-    };
-    $scope.snippet = '<span>hi there!</span>';
-});
+    }
+);
